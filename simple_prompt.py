226b45c8b26b614555065dac9eb62b53123f130b
Ahmad Hamze <ahmadhamze@yahoo.com>
2025-03-06 01:57:40 +0200

    Changes to simple_prompt.py

--- simple_prompt.py
+++ simple_prompt.py
@@ -1,14 +1,14 @@
 from openai import OpenAI
-from project_secrets import API_KEY
+from project_secrets import R1_API_KEY
 
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key=API_KEY,
+  api_key=R1_API_KEY,
 )
 
 def ask_model(input):
   result = client.chat.completions.create(
-    # the model used for the completion, make sure to use the correct version associated with the API key
+    # * The model used for the completion, make sure to use the correct version associated with the API key
     model="deepseek/deepseek-r1:free",
     messages=[
       {
@@ -18,7 +18,7 @@ def ask_model(input):
     ]
   )
   try:
-    return result.choices[0].message.content
+    return (result, result.choices[0].message.content)
   except Exception as e:
     return f"Something went wrong. Please try again: {e}"
 
@@ -28,7 +28,8 @@ def main():
     user_input = input("Ask anything: ")
     if user_input.lower() == "exit":
       break
-    response = ask_model(user_input)
+    _, response = ask_model(user_input)
+   
     print("Bot:", response)
 
 if __name__ == "__main__":
