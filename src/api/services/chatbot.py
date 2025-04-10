60a2c3481057a2e2f705935ad2d08043541b696e
Ahmad Hamze <ahmadhamze@yahoo.com>
2025-04-10 21:32:14 +0300

    Add services folder: create get_chatbot_response

---
+++ src/api/services/chatbot.py
@@ -0,0 +1,18 @@
+from src.chatbot.medical_chatbot import medical_chatbot
+
+def get_chatbot_response(query: str, chat_history=None):
+    """
+    Process a user query through the medical chatbot
+    
+    Args:
+        query: The user's question
+        chat_history: Optional chat history
+        
+    Returns:
+        The chatbot's response as a string
+    """
+    if chat_history is None:
+        chat_history = []
+        
+    response = medical_chatbot(query, chat_history)
+    return response
